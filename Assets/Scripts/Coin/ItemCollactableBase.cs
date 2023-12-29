aed43b196f531160ec98123af99a0755309d4bbc
BungasZungas <caiquefloureiro@gmail.com>
2023-12-29 02:52:11 -0300

    add gameplay to game

---
+++ Assets/Scripts/Coin/ItemCollactableBase.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollactableBase : MonoBehaviour
+{
+    public string compareTag = "Player";
+    public ParticleSystem particlesSystem;
+    public float timeToHide = 3;
+    public GameObject graphicItem;
+
+    [Header("Sounds")]
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (particlesSystem != null) particlesSystem.transform.SetParent(null);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.CompareTag(compareTag))
+        {
+            Collect();
+        }
+    }
+
+    protected virtual void Collect()
+    {
+        if(graphicItem != null) graphicItem.SetActive(false);
+        Invoke("HideObject", timeToHide);
+        OnCollect();
+    }
+
+    private void HideObject()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    protected virtual void OnCollect() 
+    {
+        if (particlesSystem != null) particlesSystem.Play();
+        if (audioSource != null) audioSource.Play();
+    }
+}

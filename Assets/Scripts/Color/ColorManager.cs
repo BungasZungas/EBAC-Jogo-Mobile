3711121df6223299c5aaa36dcc5d48a98e750cea
BungasZungas <caiquefloureiro@gmail.com>
2024-01-04 01:55:40 -0300

    add levels

---
+++ Assets/Scripts/Color/ColorManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZUNGAS.Core.Singleton;
+/*
+public class ColorManager : Singleton<ColorManager>
+{
+    public enum ArtType
+    {
+        TYPE_01,
+        TYPE_02
+    }
+
+    public List<Material> materials;
+    public List<ColorSetup> colorSetup;
+
+    private List<GameObject> floors;
+    private List<GameObject> objects;
+
+
+    public void ChangeColorByType(ColorManager.ArtType artType)
+    {
+        var setup = colorSetup.Find(i => i.artType == artType);
+
+        for(int i = 0; i < materials.Count; i++)
+        {
+            materials[i].SetColor("_Color", setup.colors[i]);
+        }
+
+    }
+}
+
+
+public class ColorSetup
+{
+    public ColorManager.ArtType artType;
+    public List<Color> colors;
+}*/
